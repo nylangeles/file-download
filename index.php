f2d9ae0c57c501b16ef2674a27d4e4ab939fe38e
Byron Jester Malvar Manalo <byronmanalo@umbradigitalcompany.com>
2023-06-05 20:02:14 +0800

    m

--- index.php
+++ index.php
@@ -47,17 +47,17 @@
     //     $files->downloadFile($zipFile, 'application/zip');
     // }
 
-    // echo shell_exec("sh -x git-pull.sh");
-    // echo "test";
+    // echo exec("sh -x git-pull.sh", $output);
+    // echo implode('\n', $output);
 
     class Git {
         protected $token;
         protected $owner;
         protected $repo;
 
-        public function __construct()
+        public function  ()
         {
-            $this->token = "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";
+            $this->token = "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";
             $this->owner = "umbra-byron-manalo";
             $this->repo = "file-download";
         }
@@ -141,14 +141,48 @@
 
             return $result;
         }
+
+        public function createPullRequest($main = 'main', $remote)
+        {
+            $ch = curl_init();
+
+            $url = "https://api.github.com/repos/%s/%s/pulls";
+            $url = sprintf($url, $this->owner, $this->repo);
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"title\":\"Amazing new feature\",\"body\":\"Please pull these awesome changes in!\",\"head\":\"$main\",\"base\":\"$remote\"}");
+
+            $headers = array();
+            $headers[] = 'Accept: application/vnd.github+json';
+            $headers[] = 'Authorization: Bearer ' . $this->token;
+            $headers[] = 'X-Github-Api-Version: 2022-11-28';
+            $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+            $headers[] = 'User-Agent: ' . $this->repo;
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+            $result = curl_exec($ch);
+            if (curl_errno($ch)) {
+                echo 'Error:' . curl_error($ch);
+            }
+            curl_close($ch);
+
+            return $result;
+        }
+
+        public function mergePullRequest()
+        {
+
+        }
     }
 
     $git = new Git();
 
-    $output = $git->mergeBranch('test-new', 'main');
+    // $output = $git->syncForkBranch('test-new');
 
-    print_r($output);
-
+    // print_r($output);
+    
     
 
 ?>
